9e3ecf3ffa521d10d4b5bc32d393e078e67409ef
AidanInceer <aidaninceer0@gmail.com>
2022-08-03 22:51:58 +0100

    basic comit

---
+++ tests/test_core/test_move.py
@@ -0,0 +1,11 @@
+from betterchess.core.move import Move
+from unittest import TestCase
+import pytest
+
+
+class TestMove(TestCase):
+    def test_castling_type_bl(self):
+        piece = "king"
+        move_col = "black"
+        str_ml = "e8c8"
+        assert "black_long" == Move.castling_type(piece, move_col, str_ml)
