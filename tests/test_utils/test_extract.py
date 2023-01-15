00bb0cc2c2ce9d5cc9d7c09e537dd7665b5ca7f0
AidanInceer <aidaninceer0@gmail.com>
2023-01-15 20:44:42 +0000

    test suite setup

---
+++ tests/test_utils/test_extract.py
@@ -0,0 +1,10 @@
+import unittest
+from datetime import datetime
+from unittest.mock import patch
+
+from betterchess.utils.extract import Extract
+
+
+class TestExtract(unittest.TestCase):
+    def test_thing(self):
+        pass
