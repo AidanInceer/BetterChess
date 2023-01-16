5696bdd310e20cb064b784b63f5f8d695dbc5424
AidanInceer <aidaninceer0@gmail.com>
2023-01-16 21:03:04 +0000

    partial completion of Config class test

--- betterchess/utils/config.py
+++ betterchess/utils/config.py
@@ -4,14 +4,11 @@ from yaml import SafeLoader
 
 
 class Config:
-    def __init__(self) -> None:
-        self.config_path = self.set_config_path()
-        self.conf = self.create_config()
-
     def set_config_path(self):
-        return r"./config/datasets.yaml"
+        self.path = r"./config/datasets.yaml"
+        return self.path
 
     def create_config(self):
-        with open(self.config_path) as f:
-            config = Box(yaml.load(f, Loader=SafeLoader))
-        return config
+        with open(r"./config/datasets.yaml") as f:
+            self.config = Box(yaml.load(f, Loader=SafeLoader))
+        return self.config
--- tests/test_utils/test_config.py
+++ tests/test_utils/test_config.py
@@ -1,11 +1,16 @@
 import unittest
-from unittest.mock import MagicMock, patch
-
-import pytest
+from unittest.mock import mock_open, patch
 
 from betterchess.utils.config import Config
 
 
-class TestConfig:
-    def test1(self):
-        assert 1 == 1
+class TestConfig(unittest.TestCase):
+    def setUp(self):
+        self.path = r"./config/datasets.yaml"
+
+    def test_set_config_path(self):
+        assert Config.set_config_path(self) == self.path
+
+    @patch("builtins.open", new_callable=mock_open, read_data="data")
+    def test_create_config(self, mock_file):
+        assert open(self.path).read() == "data"
