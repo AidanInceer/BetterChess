a2097edd38b43379419e8db29ff93095b6565876
AidanInceer <aidaninceer0@gmail.com>
2023-01-20 19:07:16 +0000

    rename cicd + fix test

--- tests/test_utils/test_extract.py
+++ tests/test_utils/test_extract.py
@@ -2,8 +2,11 @@ import unittest
 from unittest.mock import MagicMock, patch
 
 import pytest
+
 from betterchess.utils.extract import Extract
+
 
 class TestExtract:
     def test1(self):
-        assert 1 == 1
+        one = 1
+        assert one == 1
