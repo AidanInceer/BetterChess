e397548a622559be4e49dfb2622bf1daf3925efc
AidanInceer <aidaninceer0@gmail.com>
2023-01-30 22:15:36 +0000

    config docstrings

--- betterchess/utils/config.py
+++ betterchess/utils/config.py
@@ -7,12 +7,20 @@ from yaml import SafeLoader
 
 @dataclass
 class Config:
+    """Creates the config base on the specified config filepath."""
+
     path: str = r"./config/datasets.yaml"
 
     def set_config_path(self):
+        """sets the config file path."""
         return self.path
 
-    def create_config(self):
+    def create_config(self) -> Box:
+        """Loads the yaml file and creates the `Box` config object.
+
+        Returns:
+            conf (Box): config `Box` object.
+        """
         with open(self.path) as f:
             self.conf = Box(yaml.load(f, Loader=SafeLoader))
         return self.conf
