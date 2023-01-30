a4b263f6996c7d4cebd32c8238940b3c0836ab58
AidanInceer <aidaninceer0@gmail.com>
2023-01-30 22:13:24 +0000

    managers doc strings

--- betterchess/data_manager/managers.py
+++ betterchess/data_manager/managers.py
@@ -11,11 +11,19 @@ from betterchess.utils.handlers import InputHandler
 
 @dataclass
 class MySQLManager:
+    """Manager for the mysql database - allows the user to reset the databse & logs,
+    select the head of all database tables and see the size of all the tables.
+
+    Returns:
+        None
+    """
+
     config: Config
     conn: MySQLConnection
     input_handler: InputHandler
 
     def query_selector(self):
+        """Selects the query which the user specifies."""
         selection = input(
             "Choose from the following list of options (reset, size, head): "
         )
@@ -28,11 +36,12 @@ class MySQLManager:
             self.select_head_all_tables()
 
     def reset_logs(self, folder: str, user: str):
+        """Resets the logfile for the current user."""
         filename = rf"{user}.log"
         os.remove(f"{folder}/{filename}")
 
     def reset_database(self):
-
+        """Resets the database - deletes all tables and recreates empty versions."""
         curs = self.conn.cursor()
         queries = [
             self.config.conf.mysql.drop_game_table.file_path,
@@ -47,9 +56,10 @@ class MySQLManager:
             curs.execute(sql)
             self.conn.commit()
         self.conn.close()
-        print("database reset")
+        print("atabase reset")
 
     def view_table_size(self):
+        """Views all the tables and return the number of rows in each."""
         curs = self.conn.cursor()
         queries = [
             (self.config.conf.mysql.select_game_data_all.file_path, "game_data rows"),
@@ -63,6 +73,7 @@ class MySQLManager:
         self.conn.close()
 
     def select_head_all_tables(self):
+        """Select the head of all the tables."""
         curs = self.conn.cursor()
         queries = [
             self.config.conf.mysql.select_game_data.file_path,
@@ -78,7 +89,15 @@ class MySQLManager:
             print("-------------------------------------------------")
         self.conn.close()
 
-    def _get_sql_file(self, sqlfilepath):
+    def _get_sql_file(self, sqlfilepath: str):
+        """Get an sql query for the specified filepath.
+
+        Args:
+            sqlfilepath (str): sqlfile path.
+
+        Returns:
+            sql (str): sql query
+        """
         with open(sqlfilepath) as file:
             sql = file.read()
         return sql
@@ -86,11 +105,20 @@ class MySQLManager:
 
 @dataclass
 class SQLiteManager:
+
+    """Manager for the mysql database - allows the user to reset the databse & logs,
+    select the head of all database tables and see the size of all the tables.
+
+    Returns:
+        None
+    """
+
     config: Config
     conn: Connection
     input_handler: InputHandler
 
     def query_selector(self):
+        """Selects the query which the user specifies."""
         selection = input(
             "Choose from the following list of options (reset, size, head): "
         )
@@ -103,11 +131,12 @@ class SQLiteManager:
             self.select_head_all_tables()
 
     def reset_logs(self, folder: str, user: str):
+        """Resets the logfile for the current user."""
         filename = rf"{user}.log"
         os.remove(f"{folder}/{filename}")
 
     def reset_database(self):
-
+        """Resets the database - deletes all tables and recreates empty versions."""
         curs = self.conn.cursor()
         queries = [
             self.config.conf.sqlite.drop_game_table.file_path,
@@ -125,6 +154,7 @@ class SQLiteManager:
         print("database reset")
 
     def view_table_size(self):
+        """Views all the tables and return the number of rows in each."""
         curs = self.conn.cursor()
         queries = [
             (
@@ -144,6 +174,7 @@ class SQLiteManager:
         self.conn.close()
 
     def select_head_all_tables(self):
+        """Select the head of all the tables."""
         curs = self.conn.cursor()
         queries = [
             self.config.conf.sqlite.select_game_data.file_path,
@@ -160,6 +191,14 @@ class SQLiteManager:
         self.conn.close()
 
     def _get_sql_file(self, sqlfilepath):
+        """Get an sql query for the specified filepath.
+
+        Args:
+            sqlfilepath (str): sqlfile path.
+
+        Returns:
+            sql (str): sql query
+        """
         with open(sqlfilepath) as file:
             sql = file.read()
         return sql
