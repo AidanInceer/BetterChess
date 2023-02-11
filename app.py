d3c1d77249bb2e7106f70575ac19f09b03ab86a1
AidanInceer <aidaninceer0@gmail.com>
2023-02-11 22:34:48 +0000

    app update

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 from flask import Flask
+from flask_wtf.csrf import CSRFProtect
 
 app = Flask(__name__)
+csrf = CSRFProtect()
+csrf.init_app(app)
 
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route("/example/", methods=["POST"])
+@csrf.exempt  # Sensitive
+def example():
+    return "example "
